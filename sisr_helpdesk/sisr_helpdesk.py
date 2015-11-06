3f4c1637fa220b70887bdb96ee9bb645206e8629
Dony Hernandez <dohernandez@inces.gob.ve>
2015-11-06 15:30:35 -0430

    Se cambio el nombre del campo denominación a Descripción corta

--- sisr_helpdesk/sisr_helpdesk.py
+++ sisr_helpdesk/sisr_helpdesk.py
@@ -39,7 +39,7 @@ class sisr_helpdesk_incidencia(osv.osv):
         'state': fields.selection([('asignado','Asignado'),('proceso','En Proceso'),('atendido','Atendido'),('resuelto','Resuelto'),('cancelado','Cancelado')], string="Status", default="asignado"),
         'observacion_ids': fields.one2many('sisr.helpdesk.observacion', 'incidencia_id', string="Observaciones", help='Observaciones de una incidencia'),
 	'asignacion' : fields.many2one('res.users', 'Asignado a:'),
-	'denominacion': fields.char('Denominación', size=90),
+	'denominacion': fields.char('Descripción Corta', size=90),
     'forma_de_solicitud':fields.selection([('memo','Memo'),('correo','Correo Electrónico'),('llamada','Llamada Telefónica'),('presencial','Presencial'), ('gestion','Gestión Documental')], string="Forma de Solicitud"),
     #Para adjuntar los documentos a enviar.
     'adjunto' : fields.one2many('sisr.helpdesk.adjuntos', 'adjunto_id', string ="Adjuntos", help='Documentos adicionales, Respaldos Fisicos'),
@@ -137,8 +137,8 @@ class sisr_helpdesk_adjuntos(osv.osv):
     _columns = {
 
         'adjunto' : fields.binary(string="Adjuntos", help='Se suben los archivos adicionales que guardan relacion con el documento'),
-        'observacion' : fields.text(string="Descripcion", help='Breves notas sobre el archivo que se adjunta'),
-        'nombre' : fields.char(string="Descripcion", size=50, help='Breves notas sobre el archivo que se adjunta'),
+        'observacion' : fields.text(string="Descripción", help='Breves notas sobre el archivo que se adjunta'),
+        'nombre' : fields.char(string="Descripción", size=50, help='Breves notas sobre el archivo que se adjunta'),
         'adjunto_id' : fields.many2one('sisr.helpdesk.incidencia', 'incidencia'),
 }
 sisr_helpdesk_adjuntos()
